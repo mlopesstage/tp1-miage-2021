e4f2b6d4185e2873d4fc3b1b62efdf72256b9933
tp <tp@tp>
2021-03-06 19:23:11 +0100

    Version 1 test ex 3

--- todolist-debut-ex2/src/test/java/com/acme/todolist/TodolistApplicationTests.java
+++ todolist-debut-ex2/src/test/java/com/acme/todolist/TodolistApplicationTests.java
@@ -1,27 +1,38 @@
 package com.acme.todolist;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.acme.todolist.configuration.TodolistApplication;
 import com.acme.todolist.domain.TodoItem;
 
-@SpringBootTest
+@SpringBootTest(classes = TodolistApplication.class)
 class TodolistApplicationTests {
 
-	private String LATE = "[LATE!]";
+Boolean jspkoifer;
 	@Test
-	void TestIsLate(TodoItem item) {
-		//if(Instant.now().isAfter(item.getTime().plus(1, ChronoUnit.DAYS)))
-			
+	void contextLoads() {
 		
 	}
+	
+	@Test
+	@ParameterizedTest
+	void TestIsLate(TodoItem item) {
+		
+		jspkoifer = Instant.now().isAfter(item.getTime().plus(1, ChronoUnit.DAYS));
+		assertTrue(jspkoifer, "LATE");	
+					
+	}
 
-	private String finalContent(TodoItem item) {
+	/*private String finalContent(TodoItem item) {
 		return (Instant.now().isAfter(item.getTime().plus(1, ChronoUnit.DAYS))) ? 
 				LATE + item.getContent()
 				: item.getContent();
-	}
+	}*/
 }
