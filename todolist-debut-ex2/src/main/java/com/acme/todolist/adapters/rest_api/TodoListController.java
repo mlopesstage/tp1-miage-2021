adf32950611fb612d9869ade3d1950f70c58f2b1
tp <tp@tp>
2021-03-06 18:31:27 +0100

    Réalisation de l'ex2, commence ex3

--- todolist-debut-ex2/src/main/java/com/acme/todolist/adapters/rest_api/TodoListController.java
+++ todolist-debut-ex2/src/main/java/com/acme/todolist/adapters/rest_api/TodoListController.java
@@ -1,12 +1,16 @@
 package com.acme.todolist.adapters.rest_api;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 import javax.inject.Inject;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.acme.todolist.application.port.in.AddTodoItem;
@@ -24,11 +28,12 @@ public class TodoListController {
 	
 	
 	private GetTodoItems getTodoItemsQuery;
-	private AddTodoItem addstp;
+	private AddTodoItem addTodoItemsQuery;
 	
 	@Inject
-	public TodoListController(GetTodoItems getTodoItemsQuery ) {
+	public TodoListController(GetTodoItems getTodoItemsQuery, AddTodoItem addTodoItemsQuery ) {
 		this.getTodoItemsQuery = getTodoItemsQuery;
+		this.addTodoItemsQuery = addTodoItemsQuery;
 	}
 	
 	@GetMapping("/todos")
@@ -37,8 +42,11 @@ public class TodoListController {
 	}
 	
 	@PostMapping("/todos")
+	@ResponseStatus(code = HttpStatus.CREATED)
 	public void ajouterItem(@RequestBody TodoItem item) {
-		this.addstp.addTodoItem(item);
+		this.addTodoItemsQuery.addTodoItem(item);
 	}
+	
+
 	
 }
--- todolist-debut-ex2/src/test/java/com/acme/todolist/TodolistApplicationTests.java
+++ todolist-debut-ex2/src/test/java/com/acme/todolist/TodolistApplicationTests.java
@@ -1,13 +1,27 @@
 package com.acme.todolist;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.acme.todolist.domain.TodoItem;
 
 @SpringBootTest
 class TodolistApplicationTests {
 
+	private String LATE = "[LATE!]";
 	@Test
-	void contextLoads() {
+	void TestIsLate(TodoItem item) {
+		//if(Instant.now().isAfter(item.getTime().plus(1, ChronoUnit.DAYS)))
+			
+		
 	}
 
+	private String finalContent(TodoItem item) {
+		return (Instant.now().isAfter(item.getTime().plus(1, ChronoUnit.DAYS))) ? 
+				LATE + item.getContent()
+				: item.getContent();
+	}
 }
