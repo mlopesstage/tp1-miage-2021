ae181d75303a7fe2b3210ecef328c93e6136c6b5
mlopesstage <mica-lopes@dhotmail.fr>
2021-03-09 20:13:32 +0100

    Fin de l'exercice 3 (ajout de 3 tests Junit) A revoir (besoin de re lecture)

--- todolist-debut-ex2/src/test/java/com/acme/todolist/TodolistApplicationTests.java
+++ todolist-debut-ex2/src/test/java/com/acme/todolist/TodolistApplicationTests.java
@@ -1,39 +1,36 @@
 package com.acme.todolist;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.acme.todolist.configuration.TodolistApplication;
 import com.acme.todolist.domain.TodoItem;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 
 @SpringBootTest(classes = TodolistApplication.class)
 class TodolistApplicationTests {
 
-Boolean jspkoifer;
 	@Test
-	void contextLoads() {
-		
+	void itemIsLate24Hours() { //Test si la date est en retard de 24h
+		TodoItem item = new TodoItem("1", Instant.now().minus(23,ChronoUnit.HOURS), "mycontent");
+		assertThat(item.finalContent(),item.isLate(),is(false));
 	}
 	
 	@Test
-	@ParameterizedTest
-	void TestIsLate(TodoItem item) {
-		
-		jspkoifer = Instant.now().isAfter(item.getTime().plus(1, ChronoUnit.DAYS));
-		assertTrue(jspkoifer, "LATE");	
-					
+	void itemContentIsNotEmpty() { //Test que le champ content soit bien rempli
+		TodoItem item = new TodoItem("1", Instant.now().minus(23,ChronoUnit.HOURS), "mycontent");
+		assertThat("[Content is empty!]",item.getContent().isEmpty(),is(false));
 	}
-
-	/*private String finalContent(TodoItem item) {
-		return (Instant.now().isAfter(item.getTime().plus(1, ChronoUnit.DAYS))) ? 
-				LATE + item.getContent()
-				: item.getContent();
-	}*/
+	
+	@Test
+	void itemIdIsEgreaters0Empty() { //Test si la taille de l'ID est bien supérieure ou égale à 8
+		TodoItem item = new TodoItem("12345678", Instant.now().minus(23,ChronoUnit.HOURS), "mycontent");
+		assertThat("[ID is too short!]",item.getId().length() < 8,is(false));
+	}
 }
